823371dc51bafe8021e33154295737e7017b8330
Venkatesh <venkatesh.atchutha09@gmail.com>
2022-05-14 23:28:33 +0530

    initial comit

---
+++ evetManagement/src/main/java/com/eventmgt/evetManagement/EvetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.eventmgt.evetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvetManagementApplication.class, args);
+	}
+
+}
---
+++ evetManagement/src/test/java/com/eventmgt/evetManagement/EvetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventmgt.evetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
